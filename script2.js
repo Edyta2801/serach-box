40b74bdedd01ea1d7e9ad558b1e14d5dd5c4876c
Edyta2801 <edyta.szarowska@interia.pl>
2020-07-19 23:45:43 +0200

    modify

--- script2.js
+++ script2.js
@@ -1,15 +1,47 @@
-function addId(id) {
-    return function iter(o) {
-        if ('title' in o) {
-            o.id = id++;
-        }
-        Object.keys(o).forEach(function (k) {
-            Array.isArray(o[k]) && o[k].forEach(iter);
-        });
-    };
+// function addId(id) {
+//     return function iter(o) {
+//         if ('title' in o) {
+//             o.id = id++;
+//         }
+//         Object.keys(o).forEach(function (k) {
+//             Array.isArray(o[k]) && o[k].forEach(iter);
+//         });
+//     };
+// }
+
+// let data =('./data.json');
+
+// data.forEach(addId(1))
+// console.log(data);
+
+var iterator = 0; // this is going to be your identifier
+
+function addIdentifier(target){
+  target.id = iterator;
+  iterator++;
 }
 
-let data =('./data.json');
+function loop(obj){
 
-data.forEach(addId(1))
-console.log(data);
+  for(let i in obj){
+
+    var c = obj[i];
+
+    if(typeof c === 'object'){
+
+      if(c.length === undefined){
+
+        //c is not an array
+        addIdentifier(c);
+
+      }
+
+      loop(c);
+
+    }
+
+  }
+
+}
+
+loop(json);
